4cf9071023deb7eadf0fce2100414f73c2f94d8b
66993148+tugsadocal8@users.noreply.github.com <66993148+tugsadocal8@users.noreply.github.com>
2022-06-23 16:01:03 +0300

    First commit

---
+++ Assets/Scripts/Floater.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float depthBeforeSubmerged = 1f;
+    [SerializeField] private float displacementAmount = 3f;
+    [SerializeField] private int floaterCount = 1;
+    [SerializeField] private float waterDrag = 0.99f;
+    [SerializeField] private float waterAngularDrag = 0.5f;
+
+    private WavesGenerator wavesGenerator;
+
+    private void Awake() => wavesGenerator = FindObjectOfType<WavesGenerator>();
+
+    private void FixedUpdate()
+    {
+        rb.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = wavesGenerator.GetWaterHeight(transform.position);
+        if (transform.position.y < waveHeight)
+        {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
